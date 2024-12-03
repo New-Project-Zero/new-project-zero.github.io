2ff95a03306d0d69ed2730f72d5235b581e970d5
doctorLightning <doc@doclightning.net>
2024-12-03 00:00:32 -0600

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/New-Project-Zero/new-project-zero.github.io/'
 })
